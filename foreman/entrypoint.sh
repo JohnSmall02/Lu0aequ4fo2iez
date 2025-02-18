4d07eb51f5a7e336714ff23f0c84045eb88ee91d
JohnSmall02 <Johann.Klein@bmw.de>
2025-02-18 08:35:56 +0100

    initial action, config

--- foreman/entrypoint.sh
+++ foreman/entrypoint.sh
@@ -6,8 +6,5 @@ export PATH=~/bin:${GEM_HOME}/bin:${PATH}
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f ~/pids/server.pid
 
-ln -sf ./config/database.yml /etc/foreman/database.yml
-ln -sf ./config/settings.yml /etc/foreman/settings.yml
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
